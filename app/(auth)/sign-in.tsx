139ef4c522c4412493262a0f58a67d39966a54b8
Alphayo Wakarindi <alphayowakarindi@gmail.com>
2024-11-18 22:16:15 +0300

    Add sign-in screen

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,13 +1,66 @@
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import CustomButton from "@/components/CustomButton";
+import InputField from "@/components/InputField";
+import OAuth from "@/components/OAuth";
+import { icons, images } from "@/constants";
+import { Link } from "expo-router";
+import { useState } from "react";
+import { Image, ScrollView, Text, View } from "react-native";
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onSignInPress = async () => {};
+
   return (
-    <SafeAreaView>
-      <View>
-        <Text className="text-3xl text-red-500">SignIn </Text>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="absolute text-2xl text-black font-JakartaSemiBold bottom-5 left-5">
+            Welcome 👋
+          </Text>
+        </View>
+        <View className="p-5">
+          <InputField
+            label="Email"
+            placeholder="Enter your email"
+            icon={icons.email}
+            value={form.email}
+            onChangeText={(value) => setForm({ ...form, email: value })}
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter your password"
+            icon={icons.lock}
+            secureTextEntry={true}
+            value={form.password}
+            onChangeText={(value) => setForm({ ...form, password: value })}
+          />
+          <CustomButton
+            title="Sign in"
+            onPress={onSignInPress}
+            className="mt-6"
+          />
+
+          <OAuth />
+
+          <Link
+            href="/(auth)/sign-up"
+            className="mt-10 text-lg text-center text-general-200"
+          >
+            <View>
+              <Text>Don't have an account?</Text>
+            </View>
+            <View>
+              <Text className="ml-1 text-primary-500">Sign Up</Text>
+            </View>
+          </Link>
+        </View>
       </View>
-    </SafeAreaView>
+    </ScrollView>
   );
 };
 export default SignIn;
