849761c3b1c11e1ac40ab6764b59d15442941644
Alphayo Wakarindi <alphayowakarindi@gmail.com>
2024-11-18 20:19:22 +0300

    Add sign-up screen

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,13 +1,74 @@
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import CustomButton from "@/components/CustomButton";
+import InputField from "@/components/InputField";
+import OAuth from "@/components/OAuth";
+import { icons, images } from "@/constants";
+import { Link } from "expo-router";
+import { useState } from "react";
+import { Image, ScrollView, Text, View } from "react-native";
 
 const SignUp = () => {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const onSignUpPress = async () => {};
+
   return (
-    <SafeAreaView>
-      <View>
-        <Text className="text-3xl text-red-500">SignUp </Text>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="absolute text-2xl text-black font-JakartaSemiBold bottom-5 left-5">
+            Create your account
+          </Text>
+        </View>
+        <View className="p-5">
+          <InputField
+            label="Name"
+            placeholder="Enter your name"
+            icon={icons.person}
+            value={form.name}
+            onChangeText={(value) => setForm({ ...form, name: value })}
+          />
+          <InputField
+            label="Email"
+            placeholder="Enter your email"
+            icon={icons.email}
+            value={form.email}
+            onChangeText={(value) => setForm({ ...form, email: value })}
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter your password"
+            icon={icons.lock}
+            secureTextEntry={true}
+            value={form.password}
+            onChangeText={(value) => setForm({ ...form, password: value })}
+          />
+          <CustomButton
+            title="Sign Up"
+            onPress={onSignUpPress}
+            className="mt-6"
+          />
+
+          <OAuth />
+
+          <Link
+            href="/(auth)/sign-in"
+            className="mt-10 text-lg text-center text-general-200"
+          >
+            <View>
+              <Text>Already have an account?</Text>
+            </View>
+            <View>
+              <Text className="ml-1 text-primary-500">Log In</Text>
+            </View>
+          </Link>
+        </View>
       </View>
-    </SafeAreaView>
+    </ScrollView>
   );
 };
 export default SignUp;
